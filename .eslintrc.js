c0a080ceb7cad7144384b70c3445c7bc934533a7
Shabeeb <mail@shabeebk.com>
2017-07-25 12:41:57 +0530

    Update .eslintrc.js
    removed line-brake style in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,13 @@ module.exports = {
         "jsx-a11y",
         "import"
     ],
+    env: {
+        browser: true
+    },
     "rules": {
 	  "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
 	  "quotes": ["error", "single"],
-	  "jsx-quotes": [2, "prefer-double"]
+	  "jsx-quotes": [2, "prefer-double"],
+	  "linebreak-style": 0
 	}
 };
