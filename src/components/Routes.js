dc7b729306f63e5a726e39ce27e52670a1a24265
Shabeeb <shabeeb@Shabeebs-MacBook-Pro.local>
2017-08-13 22:29:50 +0530

    folder structure changes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './home';
-import About from './about';
+import Home from './Home/Home';
+import About from './About/About';
 
-// The Main component renders one of the three provided
-// Routes (provided that one matches). Both the /roster
-// and /schedule routes will match any pathname that starts
-// with /roster or /schedule. The / route will only match
-// when the pathname is exactly the string "/"
 const Routes = () => (
   <main>
     <Switch>
